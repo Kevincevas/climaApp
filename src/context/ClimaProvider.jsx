0b2d3af219dbff274c7e1967d75641b6bd681574
“Kevincevas” <cevas102@gmail.com>
2023-11-12 19:56:52 -0500

    update

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -29,7 +29,7 @@ const ClimaProvider = ({children}) => {
 
             const appId = import.meta.env.VITE_API_KEY 
 
-            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
+            const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
             const { data } = await axios(url)
             // console.log(data[0])
             const {lat, lon} = data[0]
